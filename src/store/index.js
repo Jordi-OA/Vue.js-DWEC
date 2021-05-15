4ebbcf451ddce45b5a93a436e32c5806382da06d
Jordi Oliver Andreu <36760246+Jordi-oliver@users.noreply.github.com>
2021-05-16 00:56:42 +0200

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+    state: {
+        aplicacions: [
+            {nom: "Whatsapp", descripcio: "Una aplicació de missatgeria", instalada: true},
+            {nom: "Minecraft", descripcio: "Joc", instalada: false},
+            {nom: "Meet", descripcio: "Aplicació de videoconferències per la feina", instalada: true},
+        ]
+    },
+    mutations: {
+        afegir: function (state, aplicacio) {
+            state.aplicacions.push(aplicacio);
+        },
+        esborrar: function (state, aplicacio) {
+            const index = state.aplicacions.indexOf(aplicacio);
+            state.aplicacions.splice(index, 1);
+        },
+    },
+});
+
+export default store;
