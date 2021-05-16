b0e59a021c290d337959f94031f5156d9bb4775d
Jordi Oliver Andreu <36760246+Jordi-oliver@users.noreply.github.com>
2021-05-16 17:22:33 +0200

    ButtonCounter ADDED

---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,20 @@
+<template>
+  <button v-on:click="incrementar">M'has pitjat {{ comptador }} vegades</button>
+</template>
+<script>
+export default {
+  name: "ButtonCounter",
+  data: function () {
+    return {
+      comptador: 0,
+    };
+  },
+  methods: {
+    incrementar: function () {
+      this.comptador++;
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
--- src/components/LlistaPuntuacions.vue
+++ src/components/LlistaPuntuacions.vue
@@ -1,15 +1,35 @@
 <template>
   <div>
     <ol>
-      <li>APP - puntuació</li>
-      <li>APP - puntuació</li>
-      <li>APP - puntuació</li>
-      <li>APP - puntuació</li>
+      <li>
+        {{ aplicacio.nom }} - puntuació
+        {{ this.$store.state.totalClicks }}
+      </li>
+      <li>APP - puntuació {{ this.$store.state.totalClicks }}</li>
+      <li>APP - puntuació {{ this.$store.state.totalClicks }}</li>
+      <li>APP - puntuació {{ this.$store.state.totalClicks }}</li>
     </ol>
+    <div>
+      <Aplicacio
+        v-for="(item, index) in this.$store.state.aplicacions"
+        v-bind:key="index"
+        v-bind:aplicacio="item"
+      />
+      <ButtonCounter />
+      <ButtonCounter />
+    </div>
   </div>
 </template>
 
+
 <script>
+import ButtonCounter from "@/components/ButtonCounter";
+import Aplicacio from "@/components/Aplicacio";
+export default {
+  name: "Aplicacio",
+  props: ["aplicacio"],
+  components: { ButtonCounter, Aplicacio },
+};
 </script>
 
 <style scoped>
